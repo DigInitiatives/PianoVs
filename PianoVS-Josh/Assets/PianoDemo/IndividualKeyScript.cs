70cc259b44485ea6620895c51771223dbb4db5bd
Josh Sadoway <35233439+Codeidem@users.noreply.github.com>
2019-02-07 10:52:08 -0700

    Added Sustain

---
+++ PianoVS-Josh/Assets/PianoDemo/IndividualKeyScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndividualKeyScript : MonoBehaviour
+{
+    /*
+    * Summary:
+    * This script is attatched to a "Key" which only has a public method that plays the sound clip of the attatched audio source
+    * Date: 31/01/2019
+    * Author: Adam Warkentin
+    * Dependencies: Requires an audio source component. If the key is a white key, check the "whiteKey" bool in the editor
+    */
+
+    float timeStamp;
+    bool stamp;
+
+    //keymodel is the visual component of this obj
+    GameObject keyModel;
+
+    //keyModelRenderer is just the rendered component of keyModel
+    Renderer keyModelRenderer;
+
+    //These are the materials for how the key should look when up and down
+    Material keyUp;
+    Material keyDown;
+
+    //Variable that is set in the editor to determine if the key which this script is attatched to is a whitekey or a black key
+    public bool whiteKey;
+
+    void Start()
+    {
+        keyModel = transform.GetChild(0).gameObject;
+        keyModelRenderer = keyModel.GetComponent<Renderer>();
+
+        //Gets the necessary material for the key from the Resources folder
+        if (whiteKey)
+        {
+            keyUp = Resources.Load("Key-Up") as Material;
+            keyDown = Resources.Load("Key-Down") as Material;
+        }
+        else
+        {
+            keyUp = Resources.Load("KeySharp-Up") as Material;
+            keyDown = Resources.Load("KeySharp-Down") as Material;
+        }
+
+        keyModelRenderer.material = keyUp;
+
+        stamp = false;
+    }
+
+    void Update()
+    {
+        if (stamp)
+        {
+            TimeStamp();
+        }
+    }
+
+    //Plays the audio attatched to the key and sets its material to down
+    public void PlayNote()
+    {
+        Debug.Log("NotePlay");
+        GetComponent<AudioSource>().Play();
+        keyModelRenderer.material = keyDown;
+    }
+    //Stops the audio attatched to the key and sets its material to up
+    public void StopNote()
+    {
+        Debug.Log("NoteEnd");
+        timeStamp = 0;
+        StartCoroutine("SoundStop");
+        stamp = true;
+        keyModelRenderer.material = keyUp;
+    }
+
+    public void TimeStamp()
+    {
+        GetComponent<AudioSource>().volume -= Time.deltaTime / 4;
+    }
+
+    IEnumerator SoundStop()
+    {
+        yield return new WaitForSeconds(5);
+
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().volume = 1;
+        stamp = false;
+    }
+}
---
+++ PianoVS-Josh/Assets/Scripts/NoteSpawning.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**Created By Austin Bentley
+ * Last Edited on : Febuary 5th 2019
+ * This script is designed to spawn the notes according to the song chosen
+**/
+public class NoteSpawning : MonoBehaviour
+{
+	[SerializeField]
+	GameObject note, heldNote;
+
+	public List<GameObject> keyBoardKeys;
+
+	//The Songs Beats Per Minute
+	public float bpm, timesignature, beatTime, timestamp;
+	public int beat;
+	List<Songholder> songs;
+	List<int> holdNotes;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		//Grabs all children of the object (the Keyspawners on the Keyboard)
+		keyBoardKeys = new List<GameObject>();
+		holdNotes = new List<int>();
+		for (int c = 0; c < transform.childCount; c++)
+		{
+			keyBoardKeys.Add(transform.GetChild(c).gameObject);
+			holdNotes.Add(0);
+		}
+		songs = new List<Songholder>();
+		songs.Add(new HotCrossBuns());
+		beat = 0;
+		timestamp = Time.time;
+		bpm = 120;
+		timesignature = 4;
+		beatTime = 60 / bpm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log(keyBoardKeys.Count);
+		#region Instatiate New Notes and Starts or Stops held notes
+		//calculates the current beat number based on BPM and tracks beats
+		if (Time.time - beatTime > timestamp )
+		{
+			beat++;
+			timestamp = Time.time;
+
+			//Resets the held notes if the current beat is their end beat
+			for (int c = 0; c <= holdNotes.Count - 1; c++)
+			{
+				if (holdNotes[c] == beat)
+				{
+					holdNotes[c] = 0;
+				}
+			}
+
+			//instantiates the note if it starts on this beat
+			foreach (NoteClass notes in songs[0].songNotes)
+			{
+				if(notes.GetStart() == beat)
+				{
+					for(int c = 0; c < keyBoardKeys.Count; c++)
+					{
+						if(notes.GetKey() == c)
+						{
+							Instantiate(note, keyBoardKeys[c].transform.position, keyBoardKeys[c].transform.rotation);
+							if(notes.GetEnd() != notes.GetStart())
+							{
+								holdNotes[c] = notes.GetEnd();
+							}
+						}
+					}
+				}					
+			}
+		}
+		#endregion
+		#region Held NoteCreation
+		// Checks to see if the note is being held. If it is, then create a held note.
+		for (int c =0; c <= holdNotes.Count - 1; c++)
+		{
+			if(holdNotes[c] != 0)
+			{
+				Instantiate(heldNote, keyBoardKeys[c].transform.position, keyBoardKeys[c].transform.rotation);
+			}
+		}
+		#endregion
+	}
+}
