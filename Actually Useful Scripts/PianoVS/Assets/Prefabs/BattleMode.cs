858ce5b0f89ca439727db97e0c459469e10f00a6
gsharma9 <37318174+gsharma9@users.noreply.github.com>
2019-02-14 10:08:29 -0700

    The Battle Mode Creation
    Created a new scene with the battle mode. There are now 4 Pianos that the players can see when the game starts.

--- Actually Useful Scripts/PianoVS/Assets/Prefabs/BattleMode.cs
+++ Actually Useful Scripts/PianoVS/Assets/Prefabs/BattleMode.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BattleMode : MonoBehaviour
 {
+    //Add in all of the pianos
     public GameObject pianoFirst;
     public GameObject pianoSecond;
     public GameObject pianoThird;
@@ -13,6 +14,7 @@ public class BattleMode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //For battle mode, the pianos will be spawned in these locations
         Instantiate(pianoFirst, new Vector3(-13.55f, -5.7f, 0), Quaternion.identity);
         Instantiate(pianoSecond, new Vector3(0.49f, -5.7f, 0), Quaternion.identity);
         Instantiate(pianoThird, new Vector3(-0.53f, 7.68f, 0), Quaternion.Euler(0, 0, 180));
