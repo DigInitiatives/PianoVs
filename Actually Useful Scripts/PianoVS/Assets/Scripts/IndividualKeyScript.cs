49ffb8b02df84ff95c1a902fee60eaa74746604e
gsharma9 <37318174+gsharma9@users.noreply.github.com>
2019-02-26 10:40:30 -0700

    Battle Mode and Scoring
    This has almost most of the functionality for the Battle Mode and Scoring to work

--- Actually Useful Scripts/PianoVS/Assets/Scripts/IndividualKeyScript.cs
+++ Actually Useful Scripts/PianoVS/Assets/Scripts/IndividualKeyScript.cs
@@ -1,15 +1,23 @@
 ﻿///Author: Adam Warkentin
-///This script is attatched to a "Key" which only has a public method that plays the sound clip of the attatched audio source
+///This script is attatched to a "Key" and controls note hitting, sounds, and score provision
 ///Last Modified By: Noah Rittenhouse
-///Last Modified Date: 5-Feb-2019
-///Dependencies: Requires an audio source component.If the key is a white key, check the "whiteKey" bool in the editor
+///Last Modified Date: 14-Feb-2019
+///Dependencies: If the key is a white key, check the "whiteKey" bool in the editor
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class IndividualKeyScript : MonoBehaviour
 {
+    float timeStamp;
+    float heldNoteDistance = 0;
+    bool stamp;
+
+    public bool AI;
+    int AITimer;
+
     //keymodel is the visual component of this obj
     GameObject keyModel;
 
@@ -28,13 +36,14 @@ public class IndividualKeyScript : MonoBehaviour
     Vector3 keyPos;//The position of the key, just short form for the transform.position because I am lazy
     bool holdingNote;//Variable for looping the code involving held notes
     GameObject heldNote;//Variable to store the held notes gameobject so it can be destroyed after use
-    float holdingTimer;
+    //bool holdingScoring;//Variable that alternates if the held notes give points
+
+    PlayerDataManager playerData;
 
     void Start()
     {
         keyPos = transform.position;//Initialize transform.position of the object with this script
         holdingNote = false;
-        holdingTimer = 1.0f;
 
         keyModel = transform.GetChild(0).gameObject;
         keyModelRenderer = keyModel.GetComponent<Renderer>();
@@ -52,32 +61,110 @@ public class IndividualKeyScript : MonoBehaviour
         }
 
         keyModelRenderer.material = keyUp;
+
+        playerData = transform.parent.GetComponentInParent<PlayerDataManager>();//Set playerData variable to the keyboards script component
     }
 
     public void Update()
     {
+        if (!whiteKey)
+            Debug.DrawRay(keyPos + new Vector3(0, 1.5f, 0), Vector3.up);
+        if (AI)
+        {
+            if (whiteKey)
+            {
+                if (Physics.Raycast(keyPos + new Vector3(0, 2.5f, 0), Vector3.up, out hit, .5f) && hit.collider.gameObject.tag == "Note")//Shoots raycast from the tip of note
+                {
+                    StopNote();
+                    PlayNote();
+                    AITimer = 0;
+                }
+                else
+                {
+                    if (!Physics.Raycast(keyPos + new Vector3(0, 2.5f, 0), Vector3.up, out hit, .6f) && AITimer > 5)//Shoots raycast from the tip of note
+                    {
+                        StopNote();
+                    }
+                    else
+                    {
+                        holdingNote = true;
+                    }
+                }
+                AITimer++;
+            }
+            else
+            {
+                if (Physics.Raycast(keyPos + new Vector3(0, 1.5f, 0), Vector3.up, out hit, .6f) && hit.collider.gameObject.tag == "Note")//Shoots raycast from the tip of note
+                {
+                    StopNote();
+                    PlayNote();
+                    AITimer = 0;
+                }
+                else
+                {
+                    if (!Physics.Raycast(keyPos + new Vector3(0, 1.5f, 0), Vector3.up, out hit, .6f) && AITimer > 5)//Shoots raycast from the tip of note
+                    {
+                        StopNote();
+                    }
+                    else
+                    {
+                        holdingNote = true;
+                    }
+                }
+                AITimer++;
+            }
+        }
+        if (stamp)
+        {
+            TimeStamp();
+        }
         if (holdingNote)//If the holding note has been hit
         {
-            if (Physics.Raycast(keyPos, Vector3.up, out hit, 4))//Shoots raycast from center of note to check if the holding note is gone yet
+            if (whiteKey)
             {
-                if(hit.collider.tag == "HeldNote")
+                if (Physics.Raycast(keyPos + new Vector3(0, 2.5f, 0), Vector3.up, out hit, heldNoteDistance + 1f))//Shoots raycast from the tip of note
                 {
-                    Destroy(hit.collider.gameObject);
+                    if (hit.collider.tag == "HeldNote")//If the raycast hits a note that must be held...
+                    {
+                        Debug.Log("HELD!!!!!!");
+
+                        playerData.AddHoldingScore(0.3f);
+
+                        holdingNote = true;
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else
+                    {
+                        holdingNote = false;
+                    }
                 }
-                if (hit.collider.tag != "HeldNote")//If the raycast stops hitting the held note stop giving points
+                else
                 {
-                    StopHolding();
+                    holdingNote = false;
                 }
             }
-            else//If the raycast simply doesnt hit anything...
+            else
             {
-                StopHolding();
-            }
-            holdingTimer -= Time.deltaTime;
-            if (holdingTimer <= 0)//Every frame the timer is lowered, once it hits zero it resets to one and gives points
-            {
-                //5 Points
-                holdingTimer = 1.0f;
+                if (Physics.Raycast(keyPos + new Vector3(0, 1.5f, 0), Vector3.up, out hit, heldNoteDistance + 1f))//Shoots raycast from the tip of note
+                {
+                    if (hit.collider.tag == "HeldNote")//If the raycast hits a note that must be held...
+                    {
+                        Debug.Log("HELD!!!!!!");
+
+                        playerData.AddHoldingScore(0.3f);
+
+                        holdingNote = true;
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else
+                    {
+                        holdingNote = false;
+                    }
+                }
+                else
+                {
+                    holdingNote = false;
+                }
             }
         }
     }
@@ -85,44 +172,76 @@ public class IndividualKeyScript : MonoBehaviour
     //Plays the audio attatched to the key and sets its material to down
     public void PlayNote()
     {
+        StopCoroutine("SoundStop");
+        GetComponent<AudioSource>().volume = 1;
+        stamp = false;
         //Debug.Log("NotePlay");
         GetComponent<AudioSource>().Play();
         keyModelRenderer.material = keyDown;
 
-        if (Physics.Raycast(keyPos, Vector3.up, out hit, 8))//Shoots raycast from center of note
-        {
-            if (hit.collider.tag == "Note" || hit.collider.tag == "SharpNote")//If the raycast hits a regular or sharp note...
-            {
-                if (hit.distance <= 4 && hit.distance > 2)//Sweet spot distance
-                {
-                    Debug.Log("Sweet Spot");
-                    //100 points
-                    Destroy(hit.collider.gameObject);
-                }
-                else if (hit.distance > 4)//To far distance
-                {
-                    Debug.Log("Too far");
-                }
-                else//Not to far but not in sweet spot
-                {
+        heldNoteDistance = 0;
 
+        if (whiteKey)
+        {
+            if (Physics.Raycast(keyPos + new Vector3(0, 2.3f, 0), Vector3.up, out hit, 1))//Shoots raycast from the tip of note
+            {
+                if (hit.collider.tag == "Note" || hit.collider.tag == "SharpNote")//If the raycast hits a regular or sharp note...
+                {
+                    if (hit.distance <= .7f && hit.distance > .45f)//Sweet spot distance
+                    {
+                        holdingNote = true;
+                        playerData.AddScore(100);//Sweet spot
+                        playerData.IncreaseMultiplier(1);//Add to multiplier
+
+                        heldNoteDistance = hit.distance;//Stores the distance hit in order to check for held notes
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else if (hit.distance > .7f)//To far distance
+                    {
+                        holdingNote = true;
+                        playerData.BreakMultiplier();//No score, break multiplier
+
+                        heldNoteDistance = hit.distance;
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else//Not to far but not in sweet spot
+                    {
+                        holdingNote = true;
+                        playerData.AddScore(50);//Sweet spot
+                        heldNoteDistance = hit.distance;
+                        Destroy(hit.collider.gameObject);
+                    }
                 }
             }
-            if (hit.collider.tag == "HeldNote")//If the raycast hits a note that must be held...
+        }
+        else
+        {
+            if (Physics.Raycast(keyPos + new Vector3(0, 1.5f, 0), Vector3.up, out hit, 1))//Shoots raycast from the tip of note
             {
-                if (hit.distance <= 4 && hit.distance > 2)//Sweet spot distance
+                if (hit.collider.tag == "Note" || hit.collider.tag == "SharpNote")//If the raycast hits a regular or sharp note...
                 {
-                    //100 points
-                    holdingNote = true;
-                    Destroy(hit.collider.gameObject);
-                }
-                else if (hit.distance > 4)//To far distance
-                {
-                    Debug.Log("Too far");
-                }
-                else//Not to far but not in sweet spot
-                {
-
+                    if (hit.distance <= .7f && hit.distance > .45f)//Sweet spot distance
+                    {
+                        holdingNote = true;
+                        Debug.Log("Super Sweet Spot");
+                        //Stores the distance hit in order to check forheld notes
+                        heldNoteDistance = hit.distance;
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else if (hit.distance > .7f)//To far distance
+                    {
+                        holdingNote = true;
+                        Debug.Log("Too far");
+                        heldNoteDistance = hit.distance;
+                        Destroy(hit.collider.gameObject);
+                    }
+                    else//Not to far but not in sweet spot
+                    {
+                        holdingNote = true;
+                        Debug.Log("Too close");
+                        heldNoteDistance = hit.distance;
+                        Destroy(hit.collider.gameObject);
+                    }
                 }
             }
         }
@@ -130,10 +249,10 @@ public class IndividualKeyScript : MonoBehaviour
     //Stops the audio attatched to the key and sets its material to up
     public void StopNote()
     {
-        //Debug.Log("NoteEnd");
-        GetComponent<AudioSource>().Stop();
+        Debug.Log("NoteEnd");
+        StartCoroutine("SoundStop");
+        stamp = true;
         keyModelRenderer.material = keyUp;
-
         StopHolding();
     }
 
@@ -141,8 +260,20 @@ public class IndividualKeyScript : MonoBehaviour
     {
         if (holdingNote)//If they were holding a note, destroy the note, stop looping in update, and reset timer
         {
-            holdingTimer = 1.0f;
             holdingNote = false;
         }
+    }
+    public void TimeStamp()
+    {
+        GetComponent<AudioSource>().volume -= Time.deltaTime * 8;
+    }
+
+    IEnumerator SoundStop()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().volume = 1;
+        stamp = false;
     }
 }
