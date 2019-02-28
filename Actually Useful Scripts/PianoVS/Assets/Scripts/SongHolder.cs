0a952bfae8a2c370476df208869f67f96ce5efa7
awarkentin <46605534+awarkentin@users.noreply.github.com>
2019-02-28 09:33:23 -0700

    Cleared out old project

--- Actually Useful Scripts/PianoVS/Assets/Scripts/SongHolder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Songholder : MonoBehaviour
-{
-	public List<NoteClass> songNotes;
-	public int bpm, timeSignature;
-}
