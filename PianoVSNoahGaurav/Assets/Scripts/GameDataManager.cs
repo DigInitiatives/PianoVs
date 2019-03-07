3c61c55e3086090e7c6d07df0e329ea5845a7c67
Noah <35233508+nrittenhouse1@users.noreply.github.com>
2019-03-07 10:48:45 -0700

    BadaBing

---
+++ PianoVSNoahGaurav/Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///Author: Noah Rittenhouse
+///This script will handle data universal among the 4 players
+///Last Modified By: Noah Rittenhouse
+///Last Modified Date: 7-Mar-2019
+///Dependencies: The four notespawners MUST have the NoteSpawner tag to change their song output
+///
+public class GameDataManager : MonoBehaviour
+{
+    public static bool voteInProgress;//Bool that tracks if vote is ongoing
+    static int currentVote, totalVotes;//Vote count, and total votes sent in
+    static int aiCount;//How many AI there are active
+    static int currentSongID, newSongID;
+    static GameObject[] noteSpawners;
+
+    void Start()
+    {
+        voteInProgress = false;
+        currentVote = 0;
+        aiCount = 3;
+        currentSongID = 0;
+        newSongID = -1;
+        noteSpawners = GameObject.FindGameObjectsWithTag("NoteSpawner");
+    }
+    void Update()
+    {
+        Debug.Log(voteInProgress);
+        Debug.Log(currentVote);
+        Debug.Log(currentSongID);
+        Debug.Log(newSongID);
+        if (currentVote == (4 - aiCount))//If everyone agrees, AI doesnt vote so lower needed score in case of AI activeness
+        {
+            //VOTE PASSED
+            foreach(GameObject noteSpawner in noteSpawners)//For the 4 note spawners
+            {
+                noteSpawner.GetComponent<NoteSpawning>().ChangeSong(newSongID);//Set the song choice, reset the beat, and set the bpm and time signature to the songs
+                SetCurrentSongID(newSongID);
+            }
+            ResetVote();
+        }
+        else if(totalVotes == (4 - aiCount))//If all players have voted and failed, reset
+        {
+            ResetVote();
+        }
+    }
+
+    public static void IncrementVote(int vote)//Simply increments the vote based on the players vote choice
+    {
+        //1 = yes
+        //-1 = nos
+        currentVote += vote;//If all say yes then the current vote will be 4 unless there is AI active
+        totalVotes++;
+    }
+
+    public static void SetNewSongID(int songID)
+    {
+        newSongID = songID;
+    }
+
+    public static void SetCurrentSongID(int songID)
+    {
+        currentSongID = songID;
+    }
+
+    public static string GetSongName()
+    {
+        return noteSpawners[0].GetComponent<NoteSpawning>().songs[newSongID].GetSongName();
+    }
+
+    public void ResetVote()
+    {
+        voteInProgress = false;
+        currentVote = 0;
+        totalVotes = 0;
+        newSongID = -1;
+    }
+}
---
+++ PianoVSNoahGaurav/Assets/Scripts/PlayerMenuHandler.cs
@@ -0,0 +1,112 @@
+﻿///Author: Noah Rittenhouse
+///This script will handle all the menu button presses and what they do
+///Last Modified By: Noah Rittenhouse
+///Last Modified Date: 5-Mar-2019
+///Dependencies: 
+///
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMenuHandler : MonoBehaviour
+{
+    bool menuStatus, toggleTimerStatus, votingPanelStatus, waitingMessageStatus;
+    float toggleTimer;
+    int currentSongID;
+    [SerializeField]
+    GameObject menuPanel, menuToggleText;
+    [SerializeField]
+    GameObject dropdownMenu, votingText, votePanel, waitingMessage;
+    [SerializeField]
+    GameObject noteSpawner;
+    string songName;
+    void Start()
+    {
+        songName = "default";
+        menuStatus = false;
+        toggleTimerStatus = false;
+        votingPanelStatus = false;
+        waitingMessageStatus = false;
+        toggleTimer = 0.5f;
+    }
+    void Update()
+    {
+        menuPanel.SetActive(menuStatus);//Either enable or disable the menu
+
+        waitingMessage.SetActive(waitingMessageStatus);
+        votePanel.SetActive(votingPanelStatus);//Close vote panel
+
+        if (toggleTimerStatus)//if they are holding the button decrement the timer variable
+        {
+            toggleTimer -= Time.deltaTime;
+        }
+        menuToggleText.GetComponent<Text>().text = toggleTimer.ToString();//Just a display that shows how long they have pressed
+    }
+
+    #region Menu Activation
+    public void PressedMenuToggle()//When the player presses the menu button begin a timer, or if the menu is already open then just close it
+    {
+        if (!menuStatus)
+        {
+            toggleTimerStatus = true;//Start timer
+        }
+        else
+        {
+            menuStatus = false;//Close menu
+        }
+    }
+    public void StoppedPressingMenuToggle()//Once the player lets go of the button, if they pressed it for over a second, turn the menu on
+    {
+        toggleTimerStatus = false;//Stop timer
+        if (toggleTimer <= 0)
+        {
+            menuStatus = true;//Open menu
+        }
+        toggleTimer = 0.5f;//Reset timer
+    }
+    #endregion
+
+    #region Difficulty Selection
+
+    #endregion
+    
+
+    #region Song Selection
+    public void SelectSong()
+    {
+        if (!GameDataManager.voteInProgress)//If there is not already a vote going on...
+        {
+            int songID = dropdownMenu.GetComponent<Dropdown>().value;//Set the players choice as the songID
+            if (songID != currentSongID)//if the players choice is not already playing...
+            {
+                GameDataManager.voteInProgress = true;//Let the game know there is now a vote active
+                GameDataManager.SetNewSongID(songID);//Let the game know the ID of the voted song
+
+                votingText.GetComponent<Text>().text = "Switch to " + GameDataManager.GetSongName();
+                //
+                //Send song to vote for to other players
+                //
+                votingPanelStatus = true;
+                dropdownMenu.GetComponent<Dropdown>().interactable = false;
+            }
+        }
+    }
+
+    public void SubmitVote(bool vote)
+    {
+        //true = yes
+        //false = no
+        if(vote)//Yes
+        {
+            GameDataManager.IncrementVote(1);
+        }
+        else//No
+        {
+            GameDataManager.IncrementVote(-1);
+        }
+        waitingMessageStatus = true;//Open message
+        votingPanelStatus = false;//Close panel
+    }
+    #endregion
+}
