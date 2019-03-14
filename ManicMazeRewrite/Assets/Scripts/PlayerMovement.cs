b7fce26ce56d90074cdb853ee5bb0cd216111c1c
ANJ3LL0 <47007133+ANJ3LL0@users.noreply.github.com>
2019-03-14 10:52:46 -0600

    ManicMaze
    MadeLevel1

---
+++ ManicMazeRewrite/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed;
+
+    void Start()
+    {
+        speed = 40.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
+    }
+}
