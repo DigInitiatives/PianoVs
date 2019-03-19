d6a36da5b21a9f747b7f1aa86eddd48e876f8327
awarkentin <46605534+awarkentin@users.noreply.github.com>
2019-03-19 10:11:25 -0600

    eeeee

---
+++ PianoVSNoahVoting/Assets/Resources/Prefabs/PlayerMenuHandler.cs
@@ -0,0 +1,168 @@
+﻿///Author: Noah Rittenhouse
+///This script will handle all the menu button presses and what they do
+///Last Modified By: Noah Rittenhouse
+///Last Modified Date: 5-Mar-2019
+///Dependencies: 
+///
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMenuHandler : MonoBehaviour
+{
+    bool doOnce;//Bool just for a fake start method to only play once
+    bool menuStatus, toggleTimerStatus, votingPanelStatus, waitingMessageStatus, voted;
+    float toggleTimer;
+    int currentSongID;
+
+    GameObject menuPanel, menuToggleText;
+    GameObject dropdownMenu, votingText, votePanel, waitingMessage;
+    [SerializeField]
+    GameObject noteSpawner;
+    string songName;
+    List<string> songNames;
+    void Start()
+    {
+        songName = "default";
+        menuStatus = false;
+        toggleTimerStatus = false;
+        votingPanelStatus = false;
+        waitingMessageStatus = false;
+        voted = false;
+        toggleTimer = 0.0f;
+        doOnce = true;
+
+        songNames = new List<string>();
+        //Find children assets
+        menuPanel = transform.Find("MenuPanel").gameObject;
+        menuToggleText = transform.Find("MenuToggle/Countdown").gameObject;
+        dropdownMenu = transform.Find("MenuPanel/Dropdown").gameObject;
+        votingText = transform.Find("MenuPanel/VotingStuff/VotePanel/VoteText").gameObject;
+        votePanel = transform.Find("MenuPanel/VotingStuff/VotePanel").gameObject;
+        waitingMessage = transform.Find("MenuPanel/VotingStuff/WaitingMessage").gameObject;
+        //
+
+        menuToggleText.transform.localScale = new Vector3(menuToggleText.transform.localScale.x, 0);
+
+    }
+    void Update()
+    {
+        if (doOnce)//Do once...clear dropdown, and for every song add that songs name to the dropdown list
+        {
+            dropdownMenu.GetComponent<Dropdown>().ClearOptions();
+            foreach (Songholder song in noteSpawner.GetComponent<NoteSpawning>().songs)
+            {
+                //Debug.Log(song.GetSongName());
+                songNames.Add(song.GetSongName());
+            }
+            dropdownMenu.GetComponent<Dropdown>().AddOptions(songNames);
+            doOnce = false;
+        }
+        
+
+
+        menuPanel.SetActive(menuStatus);//Either enable or disable the menu
+
+        waitingMessage.SetActive(waitingMessageStatus);
+        votePanel.SetActive(votingPanelStatus);//Close vote panel
+
+        if (toggleTimerStatus && toggleTimer < 0.5f)//if they are holding the button decrement the timer variable
+        {
+            toggleTimer += Time.deltaTime;
+            menuToggleText.transform.localScale = new Vector3(menuToggleText.transform.localScale.x, toggleTimer / 0.5f);
+        }
+        if (toggleTimer >= 0.5f)
+        {
+            menuStatus = true;//Open menu
+            menuToggleText.transform.localScale = new Vector3(menuToggleText.transform.localScale.x, toggleTimer / 0.5f);
+        }
+
+        if (GameDataManager.voteInProgress && !voted)//If there is a vote occurring and you havent voted yet make sure the menu is on
+        {
+            VotingActive();
+        }
+        else if (!GameDataManager.voteInProgress)//If there is no vote disable shit
+        {
+            VotingInactive();
+        }
+    }
+
+    #region Menu Activation
+    public void PressedMenuToggle()//When the player presses the menu button begin a timer, or if the menu is already open then just close it
+    {
+        if (!menuStatus)
+        {
+            toggleTimerStatus = true;//Start timer
+        }
+        else
+        {
+            menuStatus = false;//Close menu
+            menuToggleText.transform.localScale = new Vector3(menuToggleText.transform.localScale.x, toggleTimer / 0.5f);
+        }
+    }
+    public void StoppedPressingMenuToggle()//Once the player lets go of the button, if they pressed it for over a second, turn the menu on
+    {
+        toggleTimerStatus = false;//Stop timer
+        if (toggleTimer >= 0.5f)
+        {
+            menuStatus = true;//Open menu
+        }
+        toggleTimer = 0.0f;//Reset timer
+        menuToggleText.transform.localScale = new Vector3(menuToggleText.transform.localScale.x, toggleTimer / 0.5f);
+    }
+    #endregion
+
+    #region Difficulty Selection
+
+    #endregion
+
+
+    #region Song Selection
+    public void SelectSong()
+    {
+        if (!GameDataManager.voteInProgress)//If there is not already a vote going on...
+        {
+            //Debug.Log("Ooga");
+            int songID = dropdownMenu.GetComponent<Dropdown>().value;//Set the players choice as the songID
+            if (songID != currentSongID)//if the players choice is not already playing...
+            {
+                currentSongID = songID;
+                GameDataManager.voteInProgress = true;//Let the game know there is now a vote active
+                GameDataManager.SetNewSongID(songID);//Let the game know the ID of the voted song
+                VotingActive();
+            }
+        }
+    }
+
+    public void SubmitVote(bool vote)
+    {
+        //true = yes
+        //false = no
+        if (vote)//Yes
+        {
+            GameDataManager.IncrementVote(1);
+        }
+        else//No
+        {
+            GameDataManager.IncrementVote(-1);
+        }
+        menuStatus = false;//Close menu
+        voted = true;//They have voted
+        waitingMessageStatus = true;//Open message
+        votingPanelStatus = false;//Close panel
+    }
+
+    public void VotingActive()
+    {
+        votingText.GetComponent<Text>().text = "Switch to " + GameDataManager.GetSongName();
+        votingPanelStatus = true;
+        dropdownMenu.GetComponent<Dropdown>().interactable = false;
+    }
+    public void VotingInactive()
+    {
+        dropdownMenu.GetComponent<Dropdown>().interactable = true;
+        voted = false;
+    }
+    #endregion
+}
---
+++ PianoVSNoahVoting/Assets/Scripts/NoteSpawning.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**Created By Austin Bentley
+ * Last Edited on : March 7th 2019
+ * This script is designed to spawn the notes according to the song chosen
+ * ChangeLog: Added ChangeSong method
+**/
+public class NoteSpawning : MonoBehaviour
+{
+    //Will call from reasource folder eventually
+
+    [SerializeField]
+	GameObject note, heldNote, blackNote, heldBlackNote, noteBar;
+
+	public List<GameObject> keyBoardKeys;
+
+	//The Songs Beats Per Minute
+	public float bpm, timesignature, beatTime, timestamp;
+	public int beat;
+	public List<Songholder> songs;
+    int songSelect = 4;
+	List<int> holdNotes, heldNoteCount;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		//Grabs all children of the object (the Keyspawners on the Keyboard)
+		keyBoardKeys = new List<GameObject>();
+		holdNotes = new List<int>();
+		heldNoteCount = new List<int>();
+		for (int c = 0; c < transform.childCount; c++)
+		{
+			keyBoardKeys.Add(transform.GetChild(c).gameObject);
+			holdNotes.Add(0);
+			heldNoteCount.Add(-15);
+		}
+		songs = new List<Songholder>();
+
+		songs.Add(new HotCrossBuns());
+		songs.Add(new ATHousandMiles());
+        songs.Add(new SongOfStorms());
+        songs.Add(new LostWoods());
+        songs.Add(new TwinkleTwinkleLittleStar());
+
+		beat = 0;
+		timestamp = Time.time;
+        bpm = songs[songSelect].GetBPM();
+		timesignature = songs[songSelect].GetTimeSignature();
+        beatTime = (60 / bpm) / 16f;
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+        if (beat == songs[songSelect].GetMaxBeat())
+        {
+            beat = 0;
+        }
+        if (beat % (timesignature * 16) == 0 || beat == 0)
+		{
+			Instantiate(noteBar, transform.position, transform.rotation);
+		}
+		//Debug.Log(keyBoardKeys.Count);
+		#region Instatiate New Notes and Starts or Stops held notes
+		//calculates the current beat number based on BPM and tracks beats
+		if (Time.time - beatTime > timestamp || beat == 0)
+		{
+			//Resets the held notes if the current beat is their end beat
+			for (int c = 0; c <= holdNotes.Count - 1; c++)
+			{
+				if (holdNotes[c] == beat)
+				{
+					holdNotes[c] = 0;
+				}
+			}
+
+			//instantiates the note if it starts on this beat
+			foreach (NoteClass notes in songs[songSelect].songNotes)
+			{
+				if (notes.GetStart() == beat)
+				{
+					for(int c = 0; c < keyBoardKeys.Count; c++)
+					{
+						if(c == 1 || c == 3 || c == 6 || c== 8 || c==10 || c== 13|| c == 15 || c == 18 || c== 20|| c == 22 
+							|| c == 25 || c == 27 || c == 30 || c == 32 || c == 34 || 37 == c|| c == 39 || c == 42 || c== 44 || c == 46)
+						{
+							if (notes.GetKey() == c)
+							{
+								GameObject instantiatedObject = Instantiate(blackNote, keyBoardKeys[c].transform.position, keyBoardKeys[c].transform.rotation);
+								if(notes.GetHand() == 2)
+								{
+									instantiatedObject.GetComponent<SpriteRenderer>().color = new Color(138, 185, 255);
+								}
+								if (notes.GetEnd() != notes.GetStart())
+								{
+									holdNotes[c] = notes.GetEnd();
+									heldNoteCount[c] = -15;
+								}
+							}
+						}
+						else
+						{
+							if (notes.GetKey() == c)
+							{
+								GameObject instantiatedObject = Instantiate(note, keyBoardKeys[c].transform.position, keyBoardKeys[c].transform.rotation);
+								if (notes.GetHand() == 2)
+								{
+									instantiatedObject.GetComponent<SpriteRenderer>().color = new Color(69, 169, 207);
+								}
+								if (notes.GetEnd() != notes.GetStart())
+								{
+									holdNotes[c] = notes.GetEnd();
+									heldNoteCount[c] = -15;
+								}
+							}
+						}
+					}
+				}					
+			}
+
+
+
+            beat++;
+			timestamp = Time.time;
+		}
+        #endregion
+        #region Held NoteCreation
+        //Checks to see if the note is being held. If it is, then create a held note.
+
+        for (int c = 0; c <= holdNotes.Count - 1; c++)
+        {
+            if (c == 1 || c == 3 || c == 6 || c == 8 || c == 10 || c == 13 || c == 15 || c == 18 || c == 20 || c == 22
+                || c == 25 || c == 27 || c == 30 || c == 32 || c == 34 || 37 == c || c == 39 || c == 42 || c == 44 || c == 46)
+            {
+                if (holdNotes[c] != 0)
+                {
+                    GameObject instantiatedObject = Instantiate(heldBlackNote, keyBoardKeys[c].transform.position, keyBoardKeys[c].transform.rotation);
+                    instantiatedObject.gameObject.GetComponent<SpriteRenderer>().sortingOrder = heldNoteCount[c];
+                    heldNoteCount[c]++;
+                }
+            }
+            else
+            {
+                if (holdNotes[c] != 0)
+                {
+                    GameObject instantiatedObject = Instantiate(heldNote, keyBoardKeys[c].transform.position, keyBoardKeys[c].transform.rotation);
+                    instantiatedObject.gameObject.GetComponent<SpriteRenderer>().sortingOrder = heldNoteCount[c];
+                    heldNoteCount[c]++;
+                }
+            }
+        }
+        #endregion
+    }
+
+    public void ChangeSong(int newSongID)
+    {
+        Debug.Log("Song ID = " + newSongID);
+        songSelect = newSongID;
+        Debug.Log("Song Select " + songSelect);
+        beat = 0;
+        bpm = songs[songSelect].GetBPM();
+        timesignature = songs[songSelect].GetTimeSignature();
+        beatTime = (60 / bpm) / 16f;
+    }
+
+
+    public void ResetHoldNotes()
+    {
+        for (int c = 0; c <= holdNotes.Count - 1; c++)
+        {
+            holdNotes[c] = 0;
+        }
+    }
+}
