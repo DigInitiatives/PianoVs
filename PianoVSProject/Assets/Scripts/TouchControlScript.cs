81652ffc4171c26757c7adf768c90d37dad00373
awarkentin <46605534+awarkentin@users.noreply.github.com>
2019-04-18 09:11:24 -0600

    Added comments to TouchControlScript

--- PianoVSProject/Assets/Scripts/TouchControlScript.cs
+++ PianoVSProject/Assets/Scripts/TouchControlScript.cs
@@ -1,7 +1,7 @@
 ﻿///Author: Adam Warkentin
 ///This script is attatched to the main camera of the scene. The purpose of this script is to take in touch input, and recognize if the touch was on a key (can be easily modified for other objects)
 ///Last Modified By: Adam Warkentin
-///Last Modified Date: 19-Mar-2019
+///Last Modified Date: 18-April-2019
 ///Dependencies: Keys need to have the tag "Key"
 ///
 using System.Collections;
@@ -16,9 +16,11 @@ public class TouchControlScript : MonoBehaviour
 
     void Start()
     {
+        //Disables the mouse functionality. This was done due to contasnt interference from unintended "right-clicks"
         Input.simulateMouseWithTouches = false;
 
         hitObj = new List<GameObject>();
+        //populates the list with empty game objects. This is done because, if the list was only populated by objects when they were touched, the touches would desynced
         for (int i = 0; i < 160; i++)
         {
             hitObj.Add(new GameObject());
@@ -27,8 +29,10 @@ public class TouchControlScript : MonoBehaviour
 
     void FixedUpdate()
     {
+        //Loops through each instance of touch input
         foreach(Touch touch in Input.touches)
         {
+            //Raycast from touch
             RaycastHit hit;
             Ray ray;
             ray = Camera.main.ScreenPointToRay(touch.position);
@@ -36,34 +40,42 @@ public class TouchControlScript : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
+                    //Determines if the object hit by the raycast is a "key"
                     if(Physics.Raycast(ray, out hit))
                     {
                         if (hitObj[touch.fingerId].GetComponent<IndividualKeyScript>() != null && hit.collider.gameObject.tag == "Key" && !hitObj.Contains(hit.collider.gameObject))
                         {
+                            //Adds the key to the list of hit objects at the index of the touch
                             hitObj[touch.fingerId] = hit.collider.gameObject;
+                            //Plays key
                             hitObj[touch.fingerId].GetComponent<IndividualKeyScript>().PlayNote(false);
                         }
                     }
                     break;
                 case TouchPhase.Moved:
+                    //Checks if the raycast is still hitting a key, and if so, is it a different key
                     if (Physics.Raycast(ray, out hit))
                     {
                         if (hit.collider.gameObject.tag == "Key" && !hitObj.Contains(hit.collider.gameObject))
                         {
                             if (hitObj[touch.fingerId].GetComponent<IndividualKeyScript>() != null)
                             {
+                                //stops the note from playing
                                 hitObj[touch.fingerId].GetComponent<IndividualKeyScript>().StopNote();
                             }
-
+                            //Adds the key to the list of hit objects at the index of the touch
                             hitObj[touch.fingerId] = hit.collider.gameObject;
+                            //Plays key
                             hitObj[touch.fingerId].GetComponent<IndividualKeyScript>().PlayNote(false);
                         }
                         else if(hit.collider.gameObject.GetComponent<IndividualKeyScript>() == null)
                         {
                             if (hitObj[touch.fingerId].GetComponent<IndividualKeyScript>() != null)
                             {
+                                //stops the note from playing
                                 hitObj[touch.fingerId].GetComponent<IndividualKeyScript>().StopNote();
                             }
+                            //clears the object associated with the touch index in the list of hit objects
                             hitObj[touch.fingerId] = new GameObject();
                         }
                     }
@@ -71,8 +83,10 @@ public class TouchControlScript : MonoBehaviour
                 case TouchPhase.Stationary:
                     break;
                 case TouchPhase.Ended:
+                    //stops the note from being played once the touch has ended
                     if(hitObj[touch.fingerId].GetComponent<IndividualKeyScript>() != null)
                     {
+                        //stops the note from playing
                         hitObj[touch.fingerId].GetComponent<IndividualKeyScript>().StopNote();
                         hitObj[touch.fingerId] = new GameObject();
                     }
